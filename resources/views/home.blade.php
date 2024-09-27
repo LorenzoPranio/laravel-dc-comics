876670449ce6909090427b38d38f1114f4db635d
LorenzoPranio <lorenzopranio02@gmail.com>
2024-09-27 15:40:43 +0200

    [UPD]Created model Comic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,12 @@
     @vite(['resources/js/app.js'])
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">DC Comics</h1>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
