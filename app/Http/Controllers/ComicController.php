ef0ae8ec576edc1e8466dac971d1794a65e8984e
LorenzoPranio <lorenzopranio02@gmail.com>
2024-09-30 17:03:13 +0200

    [UPD]Added destroy

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -96,6 +96,9 @@ class ComicController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comic = Comic::findOrFail($id);
+        $comic->delete();
+
+        return redirect()->route('comics.index')->with('success', 'Comic deleted successfully!');
     }
 }
