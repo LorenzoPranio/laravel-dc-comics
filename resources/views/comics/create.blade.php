2ac2f84ed9d27648e1666f1162316177a3a33d40
LorenzoPranio <lorenzopranio02@gmail.com>
2024-10-01 16:45:13 +0200

    [UPD]Added validation and finished the excercise

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,44 +6,53 @@
 
         <form action="{{ route('comics.store') }}" method="POST">
             @csrf
-            
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" id="title" name="title" class="form-control" required>
+                <input type="text" id="title" name="title" class="form-control @error('title') is-invalid @enderror" value="{{ old('title') }}" required>
+                @error('title')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
-
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
-                <textarea id="description" name="description" class="form-control" rows="3" required></textarea>
+                <textarea id="description" name="description" class="form-control @error('description') is-invalid @enderror" rows="3" required>{{ old('description') }}</textarea>
+                @error('description')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
-
             <div class="mb-3">
                 <label for="thumb" class="form-label">Thumbnail URL</label>
-                <input type="url" id="thumb" name="thumb" class="form-control" required>
+                <input type="url" id="thumb" name="thumb" class="form-control @error('thumb') is-invalid @enderror" value="{{ old('thumb') }}" required>
             </div>
-
             <div class="mb-3">
                 <label for="price" class="form-label">Price</label>
-                <input type="number" id="price" name="price" class="form-control" required step="0.01">
+                <input type="number" id="price" name="price" class="form-control @error('price') is-invalid @enderror" value="{{ old('price') }}" required step="0.01">
+                @error('price')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
-
             <div class="mb-3">
                 <label for="series" class="form-label">Series</label>
-                <input type="text" id="series" name="series" class="form-control" required>
+                <input type="text" id="series" name="series" class="form-control @error('series') is-invalid @enderror" value="{{ old('series') }}" required>
+                @error('series')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
-
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Sale Date</label>
-                <input type="date" id="sale_date" name="sale_date" class="form-control" required>
+                <input type="date" id="sale_date" name="sale_date" class="form-control @error('sale_date') is-invalid @enderror" value="{{ old('sale_date') }}" required>
+                @error('sale_date')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
-
             <div class="mb-3">
                 <label for="type" class="form-label">Type</label>
-                <input type="text" id="type" name="type" class="form-control" required>
+                <input type="text" id="type" name="type" class="form-control @error('type') is-invalid @enderror" value="{{ old('type') }}" required>
+                @error('type')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
-
             <button type="submit" class="btn btn-primary">Create Comic</button>
         </form>
     </div>
 @endsection
-
